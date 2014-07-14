5f243d13a7b25e5c9b25794f9af7ef8c077d308e
eveR <sonico999@gmail.com>
2014-07-13 22:57:14 -0500

    static arreglados

---
+++ static/js/app.js
@@ -0,0 +1,8 @@
+/*var app = angular.module("myapp", []);
+
+app.controller("indiciosCtrl", function ($scope) {
+
+    $scope.addIndicio = function () {
+            console.log("click");
+    }
+});*/
